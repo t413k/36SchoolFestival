eee269e85f7b36d38cd4eb244d094c440d893a00
sharp-NEKO <t413k@outlook.com>
2024-09-06 02:02:56 +0900

    last version?

--- index.php
+++ index.php
@@ -12,10 +12,10 @@
     <main class="top">
         <span>≫ 何をして<span class="red-u">遊び</span>ますか?!</span>
         
-        <div class="warn-box notify-box">
+        <!-- <div class="warn-box notify-box">
         <i class="fa-solid fa-circle-info"></i>
         <span>本日はありがとうございました．また明日も運営いたします！<br/>本日換キヨし忘れた方は，明日換キヨできます！</span>
-        </div>
+        </div> -->
         
         <div class="link-cards">
             <div class="link-card">
